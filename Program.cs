e4aebd346ba1111413d90f0b7a8b283b85f159b3
Dante4Rungun <denischuprin9619@gmail.com>
2021-11-17 12:03:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SelfAssessment
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Authorization());
+        }
+        public static SqlConnection con = new SqlConnection($@"Data Source=;Initial Catalog=Sertification;User ID=;Password={СheckMethods.P12234ar()}");
+        public static string UserMail;
+        public static string PIB;
+        public static string CategoryName;
+        public static int CategoryID;
+        public static int CurrentQuestion = 0;
+        public static bool isMailRegistred;
+        public static List<int> competenceResults;
+        public static bool exit;
+        
+    }
+}
